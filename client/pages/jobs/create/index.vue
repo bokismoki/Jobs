dea390903cbe61af6ab43c619efee96a446f41d5
bokismoki <borisbosnjak084@gmail.com>
2020-04-10 23:52:00 +0200

    Jobs Create Page

---
+++ client/pages/jobs/create/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="jobs-create px-5 py-10 sm:px-10 md:py-13 md:px-10">
+    <h1
+      class="text-white uppercase tracking-wider font-semibold text-xl xl:text-3xl xl:text-center"
+    >Create new job offer</h1>
+    <JobForm />
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Create New Job'
+  },
+  components: {
+    JobForm: () =>
+      import(/*webpackChunkName: 'job-form'*/ '~/components/JobForm')
+  },
+  asyncData({ store, redirect }) {
+    if (!store.getters.loggedIn) {
+      redirect({ name: 'index' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
