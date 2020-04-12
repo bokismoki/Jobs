d16fa7d725555e5f8368ecb6f06afeb6ae2cd101
bokismoki <borisbosnjak084@gmail.com>
2020-04-12 22:34:58 +0200

    Dashboard Page

---
+++ client/components/AdminJobItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="admin-job-item text-vblue font-semibold mb-5 md:max-w-sm md:mx-auto">
+    <div class="text-xs mb-2">{{$moment(job.created_at).fromNow()}}</div>
+    <div class="flex items-start">
+      <img
+        class="w-1/3 rounded shadow mb-5 mr-2"
+        :src="`http://localhost:8080/api/job/${job.image_path}`"
+        :alt="job.image_path"
+      />
+      <div class="w-2/3">
+        <h1 class="break-all text-lg">{{job.company.name}}</h1>
+        <a
+          :href="job.company.site"
+          rel="noopener"
+          target="_blank"
+          class="break-all text-sm underline"
+        >{{job.company.site}}</a>
+      </div>
+    </div>
+    <h1 class="text-lg mt-5">{{job.title}}</h1>
+    <a
+      :href="job.job_link"
+      rel="noopener"
+      target="_blank"
+      class="text-sm break-all underline"
+    >{{job.job_link}}</a>
+    <p class="text-sm">{{job.description}}</p>
+    <div class="mt-5">
+      <button @click="approve(job.id)" class="bg-vgreen text-white rounded px-3 py-1">Approve</button>
+      <button @click="remove(job.id)" class="ml-5 bg-red-600 text-white rounded px-3 py-1">Remove</button>
+    </div>
+    <div class="bg-black h-px opacity-25 mt-5"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminJobItem',
+  props: ['job', 'index'],
+  methods: {
+    async approve(id) {
+      const response = await this.$axios.patch(
+        `/job/${id}/approve`,
+        { email: this.job.company.email },
+        {
+          headers: {
+            'content-type': 'application/json'
+          }
+        }
+      )
+      if (response.status === 200) {
+        this.$emit('approved', this.index)
+      }
+    },
+    async remove(id) {
+      const response = await this.$axios.delete(`/job/${id}`, {
+        data: {
+          email: this.job.company.email
+        }
+      })
+      if (response.status === 200) {
+        this.$emit('removed', this.index)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/pages/dashboard/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="dashboard px-5 py-10 sm:px-10 md:py-13">
+    <h1
+      class="text-white uppercase tracking-wider font-semibold leading-snug text-xl lg:text-center xl:text-3xl"
+    >Admin Dashboard</h1>
+    <div class="bg-white max-w-screen mt-10 py-5 -mx-5 px-5 sm:px-10 sm:-mx-10">
+      <p v-if="jobs.length === 0" class="font-semibold">No jobs to be approved</p>
+      <div v-else class="grid xl:grid-cols-2 xl:mx-auto">
+        <AdminJobItem
+          :job="job"
+          :index="index"
+          v-for="(job, index) in jobs"
+          :key="job.id"
+          @approved="splice"
+          @removed="splice"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Admin Dashboard'
+  },
+  components: {
+    AdminJobItem: () =>
+      import(/*webpackChunkName: 'admin-job-item'*/ '~/components/AdminJobItem')
+  },
+  methods: {
+    splice(payload) {
+      this.jobs.splice(payload, 1)
+    }
+  },
+  async asyncData({ $axios, store, redirect }) {
+    if (!store.getters.admin) {
+      redirect({ name: 'index' })
+    }
+    const jobs = await $axios.get('/job/admin')
+    return {
+      jobs: jobs.data
+    }
+  }
+}
+</script>
+
+<style scoped>
+.grid {
+  max-width: 950px;
+}
+</style>
